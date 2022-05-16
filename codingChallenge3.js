2a79abe94a482835fb064dfaaa6ca6ef7cb152d4
lynnf <lynn.fiferjr@gmail.com>
2022-05-16 15:07:26 -0500

    adding challenge 3 to repo

---
+++ codingChallenge3.js
@@ -0,0 +1,7 @@
+function FirstReverse(str) {
+    str = str.split("").reverse().join("")                   
+    // code goes here
+    return str; }
+             
+    // keep this function call here
+    console.log(FirstReverse(readline()));
